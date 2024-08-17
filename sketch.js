82cd344fb9d4fea11f5627d05d503e0fbe4e2d0b
Emmanuel Kiptoo <118376335+Dot-theduck@users.noreply.github.com>
2024-08-17 12:16:46 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let xEdgePadding = 20; // Adjust this value to change the x-axis edge padding
 let yEdgePadding = 20; // Adjust this value to change the y-axis edge padding
 let shapes = [];
 let rockImg, paperImg, scissorImg;
-let spreadSpeed = 2; // Speed for spreading out
+let spreadSpeed = 3; // Speed for spreading out
 let shapeCountPerCluster = 7; // Number of shapes per cluster
 let roundEndTime = 0; // Time when the round should end
 let winnerDeclared = false; // Flag to check if a winner has been declared
@@ -115,7 +115,7 @@ class MorphingShape {
   constructor(x, y, type) {
     this.x = x;
     this.y = y;
-    this.size = gridSize - padding * 1.2; // Size of the image
+    this.size = gridSize - padding * 1; // Size of the image
     this.type = type;
     this.offsetX = random(-5, 6);
     this.offsetY = random(-5, 6);
